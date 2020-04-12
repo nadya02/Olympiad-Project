7ff1b7eea1b0c7a62a4f357fdd0801678e410970
nadya02 <nadiapk022@gmail.com>
2020-04-12 11:40:46 +0300

    Created Countries, Towns and Coaches tables

--- OlympicGamesDB.sql
+++ OlympicGamesDB.sql
@@ -1,1 +1,22 @@
-create databes OlympicGamesDB;
+create database OlympicGamesDB;
+GO
+use OlympicGamesDB;
+GO
+create table Countries
+(
+	id int not null unique identity, primary key(id),
+	Name varchar not null
+);
+GO
+create table Towns
+(
+	id int not null unique identity, primary key(id),
+	Name varchar not null, 
+	CountryId int not null, foreign key(id) references Countries(id)
+);
+GO
+create table Coaches
+(
+	id int not null identity, primary key(id),
+	Name varchar not null,
+);
