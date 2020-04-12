e97dc2248cbd0077a3e336c5cc789782e6b842d2
nadya02 <nadiapk022@gmail.com>
2020-04-12 12:30:57 +0300

    Scaffolded database - OlympicGamesDB

---
+++ Olympiad  project code/Olympiad  project code/Models/Towns.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Olympiad__project_code.Models
+{
+    public partial class Towns
+    {
+        public Towns()
+        {
+            Competitors = new HashSet<Competitors>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CountryId { get; set; }
+
+        public virtual Countries IdNavigation { get; set; }
+        public virtual ICollection<Competitors> Competitors { get; set; }
+    }
+}
