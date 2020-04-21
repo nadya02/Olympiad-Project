9096d3350ff0ab41d0499208fb7987ba62aa6500
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2020-04-21 18:41:16 +0300

    3Clasess

--- Olympiad  project code/Olympiad  project code/Business layer/CountriesBusiness.cs
+++ Olympiad  project code/Olympiad  project code/Business layer/CountriesBusiness.cs
@@ -24,28 +24,8 @@ namespace Olympiad__project_code.Business_layer
                 return olympicGamesDBContext.Countries.Find(id);
             }
         }
-        public void Add(Countries country)
-        {
+        
 
-            using (olympicGamesDBContext = new OlympicGamesDBContext())
-            {
-                olympicGamesDBContext.Countries.Add(country);
-                olympicGamesDBContext.SaveChanges();
-            }
-        }
-
-        public void Update(Countries country)
-        {
-            using (olympicGamesDBContext = new OlympicGamesDBContext())
-            {
-                var item = olympicGamesDBContext.Countries.Find(country.Id);
-                if (item != null)
-                {
-                    olympicGamesDBContext.Entry(item).CurrentValues.SetValues(product);
-                    olympicGamesDBContext.SaveChanges();
-                }
-            }
-        }
 
     }
 }
--- Olympiad  project code/Olympiad  project code/Business layer/SportsBusiness.cs
+++ Olympiad  project code/Olympiad  project code/Business layer/SportsBusiness.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using Olympiad__project_code.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Olympiad__project_code.Business_layer
 {
     class SportsBusiness
     {
+        private OlympicGamesDBContext olympicGamesDBContext;
+
+        public List<Sports> GetAllSports()
+        {
+            using (olympicGamesDBContext = new OlympicGamesDBContext())
+            {
+                return olympicGamesDBContext.Sports.ToList();
+            }
+        }
+        public Sports GetSportById(int id)
+        {
+            using (olympicGamesDBContext = new OlympicGamesDBContext())
+            {
+                return olympicGamesDBContext.Sports.Find(id);
+            }
+        }
+
     }
 }
