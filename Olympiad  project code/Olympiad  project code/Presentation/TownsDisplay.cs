02cb735df7958828ba5c8f0d009f6416cb48668b
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2020-04-21 20:33:46 +0300

    Display

--- Olympiad  project code/Olympiad  project code/Presentation/TownsDisplay.cs
+++ Olympiad  project code/Olympiad  project code/Presentation/TownsDisplay.cs
@@ -1,11 +1,86 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Olympiad__project_code.Business_layer;
+using Olympiad__project_code.Models;
 
 namespace Olympiad__project_code.Presentation
 {
     class TownsDisplay
     {
+        private TownsBusiness townsBusiness = new TownsBusiness();
+        private CountriesBusiness countriesBusiness = new CountriesBusiness();
 
+        public void GetAllTowns()
+        {
+            Console.WriteLine("Towns: ");
+            List<Towns> towns = townsBusiness.GetAllTowns();
+
+            if (towns.Count == 0)
+            {
+                Console.WriteLine("There are no towns in the table. ");
+            }
+            else
+            {
+                Console.WriteLine("Id  |  Name   |  Country");
+                //????????????????????????????????????
+                Console.WriteLine(new string('-', 30));
+
+                foreach (var item in towns)
+                {
+                    var country = countriesBusiness.GetCountryById(item.CountryId);
+                    Console.Writeline($"{item.Id}     {item.Name}     {country.Name}");
+                }
+            }
+
+        }
+        public void GetSportById()
+        {
+            Console.Line("Enter Sport Id to fetch:");
+            int id = int.Parse(Console.ReadLine());
+            Sports sport = sportsBusiness.GetSportById(id);
+            if (sport != null)
+            {
+                Console.WriteLine(new string('-', 40));
+                Console.Writeline($"{sport.Id}     {sport.Name}");
+                Console.WriteLine(new string('-', 40));
+            }
+
+        }
+        public void AddTown()
+        {
+            Towns town = new Towns();
+            Console.Write("Enter name");
+            town.Name = Console.ReadLine();
+            townsBusiness.AddTown(town);
+
+        }
+        //Metod v country business GetCountryByName
+
+        public void Update()
+        {
+            Console.Write("Enter Id:");
+            int id = int.Parse(Console.ReadLine());
+            Towns town = townsBusiness.Get(id);
+
+            if (sport != null)
+            {
+                Console.Write("Enter new name:");
+                town.Name = Console.ReadLine();
+
+                townsBusiness.Update(town);
+            }
+            else
+            {
+                Console.WriteLine("Town not found");
+            }
+        }
+
+            public void DeleteTownById()
+            {
+                Console.Write("Enter id:");
+                int id = int.Parse(Console.ReadLine());
+                townsBusiness.Delete(id);
+            }
     }
 }
