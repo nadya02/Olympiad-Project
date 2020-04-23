df6667ef3a2a0f712b8fdce7848205f91c9a5081
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2020-04-23 14:55:01 +0300

    Update Display.cs

--- Olympiad  project code2/Olympiad  project code/Presentation/Display.cs
+++ Olympiad  project code2/Olympiad  project code/Presentation/Display.cs
@@ -6,5 +6,12 @@ namespace Olympiad__project_code.Presentation
 {
     class Display
     {
+        private static CountriesDisplay countriesDisplay = new CountriesDisplay();
+        private static TownsDisplay countriesDisplay = new TownsDisplay();
+        private static SportsDisplay countriesDisplay = new SportsDisplay();
+        private static CoachesDisplay countriesDisplay = new CoachesDisplay();
+        private static CompetitorsDisplay countriesDisplay = new CompetitorsDisplay();
+        private static ClubsDisplay countriesDisplay = new ClubsDisplay();
+        
     }
 }
