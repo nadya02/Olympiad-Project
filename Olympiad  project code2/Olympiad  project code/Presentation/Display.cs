009c7e80a23dd20dfc42142a86e7992e749cd979
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2020-04-23 15:30:05 +0300

    Update Display.cs

--- Olympiad  project code2/Olympiad  project code/Presentation/Display.cs
+++ Olympiad  project code2/Olympiad  project code/Presentation/Display.cs
@@ -16,6 +16,36 @@ namespace Olympiad__project_code.Presentation
         public static void Input()
         {
         }
-        
+        private void ShowMenuAndTables()
+        {
+            /* Console.WriteLine(new string('-', 40));
+             Console.WriteLine(new string(' ', 18) + "MENU");
+             Console.WriteLine(new string('-', 40));
+             Console.WriteLine("1. List all entries");
+             Console.WriteLine("2. Show entry by Id");
+            // Console.WriteLine("3. Add new entry");
+             Console.WriteLine("3. Update entry");
+             Console.WriteLine("4. Delete entry by ID");
+             if()
+             Console.WriteLine("0. Exit entry");*/
+
+            Console.WriteLine(new string('-', 40));
+            Console.WriteLine(new string(' ', 18) + "Olympic Games Rio 2016");
+            Console.WriteLine(new string('-', 40));
+
+            Console.WriteLine("1. Sports");
+            Console.WriteLine("2. Countries");
+            Console.WriteLine("3. Towns");
+            Console.WriteLine("4. Competitors");
+            Console.WriteLine("5. Coaches");
+            Console.WriteLine("6. Clubs");
+            Console.WriteLine("0. Exit entry");
+
+            Console.Write("Enter the number of the choosen table: ");
+
+
+        }
+
+
     }
 }
