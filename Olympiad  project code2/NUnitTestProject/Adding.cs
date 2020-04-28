1ca2a358c7f812dd7a509b620362902c3f289fee
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2020-04-28 22:03:02 +0300

    Get

---
+++ Olympiad  project code2/NUnitTestProject/Adding.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Olympiad__project_code.Models;
+using Olympiad__project_code.Business_layer;
+using Moq;
+
+namespace NUnitTestProject
+{
+    class Adding
+    {
+        private TownsBusiness townsBusiness = new TownsBusiness();
+       // [TestCase]
+
+
+    }
+}
