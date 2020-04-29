fd4686f422636fa4b283d49177de82108f2ae4c0
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2020-04-29 19:22:46 +0300

    Update CompetitorsBusiness.cs

--- Olympiad  project code2/Olympiad  project code/Business/CompetitorsBusiness.cs
+++ Olympiad  project code2/Olympiad  project code/Business/CompetitorsBusiness.cs
@@ -22,57 +22,57 @@ namespace Olympiad__project_code.Business
 
         public List<Competitors> GetAllCompetitors()
         {
-            using (olympicGamesDBContext = new OlympicGamesDBContext())
-            {
+         //   using (olympicGamesDBContext = new OlympicGamesDBContext())
+         //   {
                 return olympicGamesDBContext.Competitors.ToList();
-            }         
+         //   }         
         }
 
         public Competitors GetCompetitorById(int id)
         {
-            using (olympicGamesDBContext = new OlympicGamesDBContext())
-            {
+          //  using (olympicGamesDBContext = new OlympicGamesDBContext())
+          //  {
                 return olympicGamesDBContext.Competitors.Find(id);
-            }             
+          //  }             
         }
 
         public Competitors GetCompetitorByName(string name)
         {
-            using (olympicGamesDBContext = new OlympicGamesDBContext())
-            {
+         //   using (olympicGamesDBContext = new OlympicGamesDBContext())
+         //   {
                 var competitor = olympicGamesDBContext.Competitors
                     .Where(c => c.FullName == name)
                     .FirstOrDefault();
                 return competitor;
-            }
+          //  }
         }
 
         public void AddCompetitors(Competitors competitors)
         {
-            using (olympicGamesDBContext = new OlympicGamesDBContext())
-            {
+       //     using (olympicGamesDBContext = new OlympicGamesDBContext())
+       //     {
                 olympicGamesDBContext.Competitors.Add(competitors);
                 olympicGamesDBContext.SaveChanges();
-            }        
+        //    }        
         }
 
         public void UpdateCompetitor(Competitors competitor)
         {
-            using (olympicGamesDBContext = new OlympicGamesDBContext())
-            {
+      //      using (olympicGamesDBContext = new OlympicGamesDBContext())
+      //      {
                 var item = olympicGamesDBContext.Competitors.Find(competitor.Id);
                 if (item != null)
                 {
                     olympicGamesDBContext.Entry(item).CurrentValues.SetValues(competitor);
                     olympicGamesDBContext.SaveChanges();
                 }
-            }           
+       //     }           
         }
 
         public void DeleteCompetitorById(int id)
         {
-            using (olympicGamesDBContext = new OlympicGamesDBContext())
-            {
+       //     using (olympicGamesDBContext = new OlympicGamesDBContext())
+       //     {
                 var item = olympicGamesDBContext.Competitors.Find(id);
                 if (item != null)
                 {
@@ -80,6 +80,6 @@ namespace Olympiad__project_code.Business
                     olympicGamesDBContext.SaveChanges();
                 }
             }       
-        }
+        //}
     }
 }
