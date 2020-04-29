d87db9974ceb9668a9796676cb0cf812271be589
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2020-04-29 19:21:34 +0300

    Update CountriesBusiness.cs

--- Olympiad  project code2/Olympiad  project code/Business/CountriesBusiness.cs
+++ Olympiad  project code2/Olympiad  project code/Business/CountriesBusiness.cs
@@ -21,27 +21,27 @@ namespace Olympiad__project_code.Business_layer
 
         public List<Countries> GetAllCountries()
         {
-            using (olympicGamesDBContext = new OlympicGamesDBContext())
-            {
+           // using (olympicGamesDBContext = new OlympicGamesDBContext())
+            //{
                 return olympicGamesDBContext.Countries.ToList();
-            }
+           // }
         }
         public Countries GetCountryById(int id)
         {
-            using (olympicGamesDBContext = new OlympicGamesDBContext())
-            {
+        //    using (olympicGamesDBContext = new OlympicGamesDBContext())
+          //  {
                 return olympicGamesDBContext.Countries.Find(id);
-            }
+          //  }
         }
         public Countries GetCountryByName(string name)
         {
-            using (olympicGamesDBContext = new OlympicGamesDBContext())
-            {
+         //   using (olympicGamesDBContext = new OlympicGamesDBContext())
+          //  {
                 var country = olympicGamesDBContext.Countries
                     .Where(c => c.Name == name)
                     .FirstOrDefault();
                 return country;
-            }
+          //  }
         }
     }
 }
