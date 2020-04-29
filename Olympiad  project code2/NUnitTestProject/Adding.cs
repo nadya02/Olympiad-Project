8caa95311388879551771fcd0c3f403f5e6336c5
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2020-04-29 19:42:26 +0300

    Update Adding.cs

--- Olympiad  project code2/NUnitTestProject/Adding.cs
+++ Olympiad  project code2/NUnitTestProject/Adding.cs
@@ -41,30 +41,8 @@ namespace NUnitTestProject
             data.ToList().ForEach(p => service.AddTown(p));
 
             Assert.AreEqual(data.ToList(), service.GetAllTowns());
-        }   [TestCase]
-        public void TestUpdateTowns()
-        {
-            var data = new List<Towns>()
-                {
-                    new Towns { Id =  1,Name = "Town1"},
-                    new Towns { Id = 2, Name = "Town2"},
-                    new Towns { Id = 3,Name = "Town3"},
-                }.AsQueryable();
-
-            var mockSet = new Mock<DbSet<Towns>>();
-            mockSet.As<IQueryable<Towns>>().Setup(m => m.Provider).Returns(data.Provider);
-            mockSet.As<IQueryable<Towns>>().Setup(m => m.Expression).Returns(data.Expression);
-            mockSet.As<IQueryable<Towns>>().Setup(m => m.ElementType).Returns(data.ElementType);
-            mockSet.As<IQueryable<Towns>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
-
-            var mockContext = new Mock<OlympicGamesDBContext>();
-            mockContext.Setup(c => c.Towns).Returns(mockSet.Object);
-
-            var service = new TownsBusiness(mockContext.Object); // service = контролер
-            data.ToList().ForEach(p => service.AddTown(p));
-
-            Assert.AreEqual(data.ToList(), service.GetAllTowns());
-        }   [TestCase]
+        }   
+        [TestCase]
         public void TestAddCompetitor()
         {
             var data = new List<Towns>()
